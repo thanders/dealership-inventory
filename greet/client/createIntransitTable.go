fa79b9546b35dbbd6a1cdce58079761d07dfd120
thanders <thanders@users.noreply.github.com>
2022-07-10 18:49:27 -0700

    feat(greet): Server and Client

---
+++ greet/client/createIntransitTable.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	pb "github.com/thanders/dealership-inventory/greet/proto"
+)
+
+func createIntransitTable(inventoryList *pb.TotalInventory) {
+
+	w := tabwriter.NewWriter(os.Stdout, 10, 0, 4, ' ', tabwriter.TabIndent)
+	fmt.Println("Intransit Inventory")
+	fmt.Fprintln(w, "Dealer Number\tModel Name\tModel Year\tIn Transit\tExterior Color\tInterior Color\tTransmission\tMSRP\t")
+	for _, v := range inventoryList.InTransitData {
+		fmt.Fprint(w, v.DealerNumber, "\t", v.ModelGroupName, "\t", v.ModelYear, "\t", v.NumberInTransit, "\t", v.ExteriorColor, "\t", v.InteriorColor, "\t", v.Transmission, "\t", v.ModelMSRP, "\t\n")
+	}
+	w.Flush()
+
+}
